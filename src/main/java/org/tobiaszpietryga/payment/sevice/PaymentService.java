daf0e00ba46632234542105c924301f0e6f4449c
Tobiasz Pietryga <tobiasz.pietryga@eo.pl>
2025-04-02 12:22:02 +0200

    confirmPaymebt

--- src/main/java/org/tobiaszpietryga/payment/sevice/PaymentService.java
+++ src/main/java/org/tobiaszpietryga/payment/sevice/PaymentService.java
@@ -25,12 +25,13 @@ public class PaymentService {
 		Optional<Customer> possibleCustomer = customerRepository.findById(order.getCustomerId());
 		if (possibleCustomer.isPresent()) {
 			Customer customer = possibleCustomer.get();
-			log.info("Found customer: {}", customer);
+			log.info("Reserve: Found customer: {}", customer);
 			if (order.getPrice().compareTo(customer.getAmountAvailable()) <= 0) {
 				order.setStatus(Status.PARTIALLY_CONFIRMED);
 				customer.setAmountReserved(customer.getAmountReserved() + order.getPrice());
 				customer.setAmountAvailable(customer.getAmountAvailable() - order.getPrice());
 				customerRepository.save(customer);
+				log.info("Reserve: customer saved: {}", customer);
 				order.setPaymentStarted(true);
 			} else {
 				order.setStatus(Status.PARTIALLY_REJECTED);
@@ -39,5 +40,26 @@ public class PaymentService {
 			order.setStatus(Status.PARTIALLY_REJECTED);
 		}
 		kafkaTemplate.send(topicName, order.getId(), order);
+		log.info("Reserve: message sent: {}", order);
+	}
+
+	public void confirmPayment(Order order) {
+		Optional<Customer> possibleCustomer = customerRepository.findById(order.getCustomerId());
+		if (possibleCustomer.isPresent()) {
+			Customer customer = possibleCustomer.get();
+			log.info("Confirm: Found customer: {}", customer);
+			if (order.getStatus().equals(Status.CONFIRMED)) {
+				customer.setAmountReserved(customer.getAmountReserved() - order.getPrice());
+				customerRepository.save(customer);
+				log.info("Confirm: customer saved{}", customer);
+			} else if (order.getStatus().equals(Status.ROLLBACK)) {
+				customer.setAmountReserved(customer.getAmountReserved() - order.getPrice());
+				customer.setAmountAvailable(customer.getAmountAvailable() + order.getPrice());
+				customerRepository.save(customer);
+				log.info("Confirm: customer saved{}", customer);
+			} else {
+				log.warn("Confirm: incorrect order status: {}", order.getStatus());
+			}
+		}
 	}
 }
